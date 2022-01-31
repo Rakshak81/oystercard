ddc95373c05d983ade92bf9241ba5ce509c326f9
Francesco <francesco@monzo.com>
2022-01-31 18:26:02 +0100

    Closing #5: top up and balance updated

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,10 +1,15 @@
 class Oystercard
   attr_reader :balance
+
   def initialize
     @balance = 0
   end
-  
-#   def balance
+
+  def top_up(recharge)
+    @balance = @balance + recharge
+  end
+
+#   def balance # replaced by attr_reader
 #   @balance
 #   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -5,4 +5,14 @@ describe Oystercard do
       expect(subject.balance).to eq(0)
     end
   end
+
+  describe "#top_up" do
+    it "Tops up the card" do
+      expect(subject).to respond_to(:top_up).with(1).argument
+    end
+
+    it "Tops up the card and returns the new Balance" do
+      expect { subject.top_up(5) }.to change { subject.balance }.by(5)
+    end
+  end
 end
